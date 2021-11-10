c41d35404b2a17b660c6e2c1806508f98a9d441a
levizard <levizardos@gmail.com>
2021-11-10 13:19:13 +0500

    Create: added slider photos; slider switch on key

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
+  },
+  
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
   },
 
   // Navigation arrows
   navigation: {
-    prevEl: '.slider-button__prev',
-    nextEl: '.slider-button__next',
+    prevEl: ".slider-button__prev",
+    nextEl: ".slider-button__next",
   },
 });
