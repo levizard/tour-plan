e945710b180022bc7fb5930e91c5a952d5df4c80
levizard <levizardos@gmail.com>
2021-12-08 11:43:03 +0500

    Create: validation for modals

--- js/main.js
+++ js/main.js
@@ -43,17 +43,11 @@ $(document).ready(function () {
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
 
-  $(document).keydown(function(event) { 
-    if (event.keyCode == 27) { 
+  $(document).keydown(function (event) {
+    if (event.keyCode == 27) {
       closeModal();
     }
   });
-/* 
-  closeModalButton.keypress(function(e) { 
-    if (e.keyCode === 27) {
-      closeModal;
-    };
-  } */
 
   function openModal() {
     var modalOverlay = $(".modal__overlay");
@@ -69,4 +63,38 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay__visible");
     modalDialog.removeClass("modal__dialog__visible");
   }
+
+  // Обработка форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Your name should be more than 2 symbols",
+        },
+        phone: {
+          required: "Please add your phone number",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+      },
+    });
+  });
+
+  $('.phone').mask('+7 (000) 000-00-00');
+
 });
